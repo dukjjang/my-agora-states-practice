fbf97bd7b1d5f584d1a7ca4b0453eff82cfa9e55
dukjjang <jhd7292@naver.com>
2022-08-08 14:40:30 +0900

    테스트 추가

---
+++ my-agora-states-client/src/App.test.js
@@ -0,0 +1,9 @@
+it("예상 동작이 실제 동작과 일치하지 않을 때 오류가 발생합니다.", function () {
+  let even = function (num) {
+    return num % 2 === 0; // 체크하려는 함수에 뭔가 문제가 있군요!
+  };
+
+  if (even(9) !== true) {
+    throw new Error("10은 짝수여야 합니다!");
+  }
+});
